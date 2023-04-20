623fab75f4c4aed3d77909a53c8cec16b72e5919
gorkavillar <gorka@gorkavillar.com>
2023-04-20 15:48:20 +0200

    Tema 09 - Preparación

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import './index.css';
-import Home from './components/Home';
-import Contacto from './components/Contacto';
-
-const routes = [
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/contacto',
-    element: <Contacto />
-  }
-]
-
-const router = createBrowserRouter(routes);
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
