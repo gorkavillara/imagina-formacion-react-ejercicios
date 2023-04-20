6488e5fb36a30c4a473a5c1ae82a3afc727f6b88
gorkavillar <gorka@gorkavillar.com>
2023-04-20 12:29:02 +0200

    Tema 07 - Preparación

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
-import { useEffect, useState, useRef } from "react";
 import "./App.scss";
-
-const Saludo = ({ nombre }) => <h1>Hola {nombre}</h1>;
-
-const ComponenteCambio = () => {
-  const [activo, setActivo] = useState(false);
-  const buttonRef = useRef(null);
-  const cambiaEstado = () => setActivo((estadoanterior) => !estadoanterior);
-
-  const obtenInfo = () => {
-    const target = buttonRef.current.target;
-    console.log(target);
-  };
-
-  return (
-    <button
-      className={`${activo ? "encendido" : "apagado"}`}
-      ref={buttonRef}
-      onMouseEnter={obtenInfo}
-      onClick={cambiaEstado}
-    >
-      {activo ? "Encendido" : "Apagado"}
-    </button>
-  );
-};
-
-const ComponenteMontado = () => {
-  useEffect(() => console.log("Se ha montado el componente"), []);
-  return <h1>Componente montado</h1>;
-};
 
 function App() {
   return (
     <div className="App">
-      <Saludo nombre="Gorka" />
-      <ComponenteCambio />
-      <ComponenteMontado />
+      Hola mundo
     </div>
   );
 }
