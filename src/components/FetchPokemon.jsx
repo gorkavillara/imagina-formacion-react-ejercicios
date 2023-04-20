cb14f24b713f6cab1892cbf7961e7c1cb0001d37
gorkavillar <gorka@gorkavillar.com>
2023-04-20 16:07:23 +0200

    Tema 09 - Resolución

---
+++ src/components/FetchPokemon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FetchPokemon = () => {
+  const buscaPokemon = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon/298')
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(e => console.error(e))
+  }
+  return <button onClick={buscaPokemon}>Busca Pokémon</button>;
+};
+
+export default FetchPokemon;
